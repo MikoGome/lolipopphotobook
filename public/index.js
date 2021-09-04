8b45c6d0d27a1fde3800f591b555ae84f01d4cce
Miko <89363492+MikoGome@users.noreply.github.com>
2021-09-04 03:28:03 -0700

    Delete index.js

--- public/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("search").addEventListener("input", () => {
-  document.querySelectorAll(".picture_element").forEach( element => {
-    element.style.display = "none";
-    if (element.getAttribute("id").includes(document.getElementById("search").value)) {
-      element.style.display = "block";
-    }
-  });
-});
